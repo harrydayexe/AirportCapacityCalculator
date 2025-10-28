1224ebe6c28e60637e31f562b8d225c6f39515bb
Harry Day <contact@harryday.xyz>
2025-10-28 18:26:00 +0000

    docs(airport): add airport docs

--- internal/airport/airport.go
+++ internal/airport/airport.go
@@ -1,5 +1,7 @@
+// Package airport provides combined airport modeling and calculations.
 package airport
 
+// Airport represents a physical airport with all its subcomponents.
 type Airport struct {
-	Name string
+	Name string // The commercial name of the airport
 }
